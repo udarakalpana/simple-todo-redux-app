5666669abb31825d2db0adcb97c5a45451ab4f65
kalpana udara <kalpanaudara058@gmail.com>
2025-01-24 20:38:20 +0530

    Installed prettier and create ui for task add.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-
+import {useState} from "react";
 
 const App = () => {
+    const [task, setTask] = useState('')
+
+    const handleInput = (event) => {
+        setTask(event.target.value)
+    }
 
   return (
-    <>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </>
-  )
-}
+      <>
+          <div>
+              <label
+                  htmlFor="small-input"
+                  className="mb-2 text-sm font-medium text-dark"
+              >
+                  Task
+              </label>
+              <input
+                  type="text"
+                  id="small-input"
+                  name="task"
+                  onChange={handleInput}
+                  className="w-1/2 m-4 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs"
+              />
+              <button
+                  type="button"
+                  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+              >
+                  Add new task
+              </button>
+          </div>
+          <div>
+              {task}
+          </div>
+      </>
 
-export default App
+  );
+};
+
+export default App;
